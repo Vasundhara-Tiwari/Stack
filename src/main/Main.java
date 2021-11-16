8235d274a7d2481656eb8c53900a3abee85a57e6
Vasundhara-Tiwari <vasundhara.tiwari_cs19@gla.ac.in>
2021-11-16 10:34:44 +0530

    calling push method of the stack from main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,12 @@
 package main;
 
+import stack.Stack;
+
 public class Main {
     public static void main(String[] args) {
-
+        Stack stack = new Stack();
+        stack.push(10);
+        stack.push(15);
+        stack.push(20);
     }
 }
